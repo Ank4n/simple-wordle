f527257bf23de7cc7f8f58e40e655c56426e71ac
theduck <ankan.anurag@gmail.com>
2022-06-13 14:45:04 +0200

    fix signature

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const YELLOW: char = '🟨';
 const WHITE: char = '⬜';
 const SEPARATOR: char = ' ';
 
-fn calc_green_and_yellow(guess: Vec<u8>, secret: &[u8]) -> String {
+fn calc_green_and_yellow(guess: &[u8], secret: &[u8]) -> String {
     assert!(
         guess.len() == secret.len(),
         "answer and solution are of different length"
@@ -19,6 +19,7 @@ fn calc_green_and_yellow(guess: Vec<u8>, secret: &[u8]) -> String {
     let mut out = String::from("");
 
     let mut i = 0;
+
     while i < secret.len() {
         if out.len() > 0 {
             out.push(SEPARATOR)
@@ -52,35 +53,34 @@ fn random_generator(max: u8) -> u8 {
 #[test]
 fn test_green_and_yellow() {
     assert_eq!(
-        calc_green_and_yellow([1, 2, 3, 4].to_vec(), &[1, 2, 3, 4]),
+        calc_green_and_yellow(&[1, 2, 3, 4], &[1, 2, 3, 4]),
         "🟩 🟩 🟩 🟩".to_string()
     );
     assert_eq!(
-        calc_green_and_yellow([1, 2, 3, 5].to_vec(), &[1, 2, 3, 4]),
+        calc_green_and_yellow(&[1, 2, 3, 5], &[1, 2, 3, 4]),
         "🟩 🟩 🟩 ⬜".to_string()
     );
     assert_eq!(
-        calc_green_and_yellow([4, 3, 2, 1].to_vec(), &[1, 2, 3, 4]),
+        calc_green_and_yellow(&[4, 3, 2, 1], &[1, 2, 3, 4]),
         "🟨 🟨 🟨 🟨".to_string()
     );
     assert_eq!(
-        calc_green_and_yellow([1, 2, 3, 1].to_vec(), &[1, 2, 3, 4]),
+        calc_green_and_yellow(&[1, 2, 3, 1], &[1, 2, 3, 4]),
         "🟩 🟩 🟩 ⬜".to_string()
     );
     assert_eq!(
-        calc_green_and_yellow([1, 1, 1, 1].to_vec(), &[1, 2, 3, 4]),
+        calc_green_and_yellow(&[1, 1, 1, 1], &[1, 2, 3, 4]),
         "🟩 ⬜ ⬜ ⬜".to_string()
     );
     assert_eq!(
-        calc_green_and_yellow([1, 2, 2, 2].to_vec(), &[2, 2, 2, 1]),
+        calc_green_and_yellow(&[1, 2, 2, 2], &[2, 2, 2, 1]),
         "🟨 🟩 🟩 🟨".to_string()
     );
     assert_eq!(
-        calc_green_and_yellow([1, 3, 3, 2].to_vec(), &[2, 2, 2, 1]),
+        calc_green_and_yellow(&[1, 3, 3, 2], &[2, 2, 2, 1]),
         "🟨 ⬜ ⬜ 🟨".to_string()
     );
 }
-
 
 fn main() {
     let stdin = stdin();
@@ -89,14 +89,14 @@ fn main() {
 
     let secret = generate_solution(random_generator);
     println!("solution: {:?}", secret);
-      
+
     loop {
         buf.clear();
         println!("guess: ");
         stdin.read_line(&mut buf).unwrap();
-        let guess : Result<Vec<u8>, _> = buf.trim().split(' ').map(|s| s.parse()).collect();
-        let guess = guess.unwrap();  
-        let squares = calc_green_and_yellow(guess.clone(), &secret);
+        let guess: Result<Vec<u8>, _> = buf.trim().split(' ').map(|s| s.parse()).collect();
+        let guess = guess.unwrap();
+        let squares = calc_green_and_yellow(&guess, &secret);
         println!("{:?} got {}", &guess, squares);
         if squares == WIN {
             print!("Congrats!! You got it all right..");
