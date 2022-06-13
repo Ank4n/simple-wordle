d9118fd035758307d68f6fd2588ba90fc98dcbe8
theduck <ankan.anurag@gmail.com>
2022-06-13 13:22:54 +0200

    wordle game

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use rand::prelude::*;
+use std::io::stdin;
+
+const WIN: &str = "🟩 🟩 🟩 🟩";
+const GREEN: char = '🟩';
+const YELLOW: char = '🟨';
+const WHITE: char = '⬜';
+const SEPARATOR: char = ' ';
+
+fn calc_green_and_yellow(guess: Vec<u8>, secret: &[u8]) -> String {
+    assert!(
+        guess.len() == secret.len(),
+        "answer and solution are of different length"
+    );
+
+    let mut secret = secret.to_vec();
+    let mut guess = guess.to_vec();
+
+    let mut out = String::from("");
+
+    let mut i = 0;
+    while i < secret.len() {
+        if out.len() > 0 {
+            out.push(SEPARATOR)
+        }
+
+        if guess[i] == secret[i] {
+            out.push(GREEN);
+            secret.remove(i);
+            guess.remove(i);
+        } else if secret.contains(&guess[i]) {
+            out.push(YELLOW);
+            i += 1;
+        } else {
+            out.push(WHITE);
+            i += 1;
+        }
+    }
+
+    out
+}
+
+fn generate_solution(random: fn(u8) -> u8) -> [u8; 4] {
+    [random(4), random(4), random(4), random(4)]
+}
+
+fn random_generator(max: u8) -> u8 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(1..max + 1)
+}
+
+#[test]
+fn test_green_and_yellow() {
+    assert_eq!(
+        calc_green_and_yellow([1, 2, 3, 4].to_vec(), &[1, 2, 3, 4]),
+        "🟩 🟩 🟩 🟩".to_string()
+    );
+    assert_eq!(
+        calc_green_and_yellow([1, 2, 3, 5].to_vec(), &[1, 2, 3, 4]),
+        "🟩 🟩 🟩 ⬜".to_string()
+    );
+    assert_eq!(
+        calc_green_and_yellow([4, 3, 2, 1].to_vec(), &[1, 2, 3, 4]),
+        "🟨 🟨 🟨 🟨".to_string()
+    );
+    assert_eq!(
+        calc_green_and_yellow([1, 2, 3, 1].to_vec(), &[1, 2, 3, 4]),
+        "🟩 🟩 🟩 ⬜".to_string()
+    );
+    assert_eq!(
+        calc_green_and_yellow([1, 1, 1, 1].to_vec(), &[1, 2, 3, 4]),
+        "🟩 ⬜ ⬜ ⬜".to_string()
+    );
+    assert_eq!(
+        calc_green_and_yellow([1, 2, 2, 2].to_vec(), &[2, 2, 2, 1]),
+        "🟨 🟩 🟩 🟨".to_string()
+    );
+    assert_eq!(
+        calc_green_and_yellow([1, 3, 3, 2].to_vec(), &[2, 2, 2, 1]),
+        "🟨 ⬜ ⬜ 🟨".to_string()
+    );
+}
+
+
+fn main() {
+    let stdin = stdin();
+
+    let mut buf = String::new();
+
+    let secret = generate_solution(random_generator);
+    println!("solution: {:?}", secret);
+      
+    loop {
+        buf.clear();
+        print!("guess: ");
+        stdin.read_line(&mut buf).unwrap();
+        let guess : Result<Vec<u8>, _> = buf.trim().split(' ').map(|s| s.parse()).collect();
+        let guess = guess.unwrap();  
+        let squares = calc_green_and_yellow(guess.clone(), &secret);
+        println!("{:?} got {}", &guess, squares);
+        if squares == WIN {
+            print!("Congrats!! You got it all right..");
+            break;
+        }
+    }
+}
